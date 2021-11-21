20b051591ff8845e925311661937fe1a8db392b4
zergrisk1 <80167158+zergrisk1@users.noreply.github.com>
2021-11-22 01:36:08 +0900

    Add files via upload

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,27 +1,32 @@
 <template>
     <div class="vue-template">
-<el-card>
-        <form @submit.prevent="login">
-            <h3>Sign In</h3>
+      <div class="bg">
+      </div>
+      <div class="sidebar">
+        <div class="side-form">
+          <form @submit.prevent="login">
+              <h3>Sign In</h3>
 
-            <div class="form-group">
-                <label>Username</label>
-                <input type="username" class="form-control form-control-lg" v-model="userForm.username"/>
-            </div>
+              <div class="form-group">
+                  <label>Username</label>
+                  <input type="username" class="form-control form-control-lg" v-model="userForm.username"/>
+              </div>
 
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" class="form-control form-control-lg" v-model="userForm.password"/>
-            </div>
+              <div class="form-group">
+                  <label>Password</label>
+                  <input type="password" class="form-control form-control-lg" v-model="userForm.password"/>
+              </div>
 
-            <button type="login" class="btn btn-dark btn-lg btn-block">Sign In</button>
+              <button type="login" class="btn btn-dark btn-lg btn-block">Sign In</button>
+          </form>
+        <form @submit.prevent="register">
+          <div class="form-group">
+            <span>Not Registered yet? </span>
+            <el-button type="text" @click="register ">Register</el-button>
+          </div>
         </form>
-      <form @submit.prevent="register">
-        <div class="form-group">
-          <button type="login" class="btn btn-black btn-lg btn-block">Register</button>
-        </div>
-      </form>
-  </el-card>
+      </div>
+     </div>
     </div>
 </template>
 
