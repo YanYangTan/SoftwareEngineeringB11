4db69d3c81fa4629a2675d3f9264b235a4883fed
zergrisk1 <80167158+zergrisk1@users.noreply.github.com>
2021-11-26 00:21:38 +0900

    Add files via upload

--- client/src/components/MainPage.vue
+++ client/src/components/MainPage.vue
@@ -3,33 +3,30 @@
 <el-container style="height: 800px; border: 1px solid #eee">
   <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
     <el-menu :default-openeds="['1', '3']">
+      <el-submenu index="2">
+        <template slot="title"><i class="el-icon-user"></i>用户</template>
+        <el-menu-item-group>
+          <el-menu-item index="2-1">个人信息</el-menu-item>
+          <el-menu-item index="2-2"><el-button type="text" style="color: crimson" @click="SignIn">退出登录</el-button></el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
       <el-submenu index="1">
         <template slot="title"><i class="el-icon-menu"></i>导航</template>
         <el-menu-item-group>
-          <el-menu-item index="1-1">群组管理</el-menu-item>
-          <el-menu-item index="1-2">动态</el-menu-item>
-          <el-menu-item index="1-2">家谱管理</el-menu-item>
+          <el-menu-item index="1-1"><el-button type="text" style="color: black" @click="TurnToGroupList">群组管理</el-button></el-menu-item>
+          <el-menu-item index="1-2">动态管理</el-menu-item>
+          <el-menu-item index="1-3">家谱管理</el-menu-item>
+          <el-menu-item index="1-4"><el-button type="text" style="color: black" @click="TurnToCalender">日历管理</el-button></el-menu-item>
         </el-menu-item-group>
       </el-submenu>
     </el-menu>
   </el-aside>
 
   <el-container>
-    <el-header style="text-align: right; font-size: 12px">
-      <el-dropdown>
-        <i class="el-icon-setting" style="margin-right: 15px"></i>
-        <el-dropdown-menu slot="dropdown">
-          <el-dropdown-item>个人信息</el-dropdown-item>
-          <el-dropdown-item><el-button type="text" style="color: crimson" @click="SignIn">退出登录</el-button></el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
-      <span>白</span>
-    </el-header>
 
     <el-main>
-          <GroupList
-
-          ></GroupList>
+      <Calender v-if="this.$data.index==='Calender'"></Calender>
+      <GroupList v-if="this.$data.index==='GroupList'"></GroupList>
     </el-main>
   </el-container>
 </el-container>
@@ -42,6 +39,7 @@
 import axios from 'axios';
 import RegisterPage from './register.vue';
 import GroupList from './GroupList.vue';
+import Calender from './Calender.vue';
 
 export default {
   name: 'MainPage',
@@ -55,12 +53,16 @@ export default {
   components: {
     // eslint-disable-next-line vue/no-unused-components
     RegisterPage,
+    // eslint-disable-next-line vue/no-unused-components
+    Calender,
+    // eslint-disable-next-line vue/no-unused-components
     GroupList,
   },
   data() {
     return {
       // userid: '',
       msg: '',
+      index: 'Calender',
     };
   },
   methods: {
@@ -76,6 +78,12 @@ export default {
     },
     SignIn() {
       this.$router.push('/');
+    },
+    TurnToGroupList() {
+      this.$data.index = 'GroupList';
+    },
+    TurnToCalender() {
+      this.$data.index = 'Calender';
     },
   },
   created() {
