923053deed3bd6b43d813d91ba8370124f48776d
KY0602 <86710042+KY0602@users.noreply.github.com>
2021-11-25 17:04:41 +0800

    完成群组管理部分

---
+++ models.py
@@ -0,0 +1,30 @@
+from . import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    idusers = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(45), unique=True)
+    password = db.Column(db.String(255))
+    email = db.Column(db.String(45), unique=True)
+    phone = db.Column(db.String(45))
+    birthday = db.Column(db.Date)
+
+    relation_group_user = db.relationship("RelationGroupUser", backref="users")
+
+
+class Group(db.Model):
+    __tablename__ = 'groups'
+    idgroups = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(45))
+    invite_key = db.Column(db.String(45))
+
+    relation_group_user = db.relationship("RelationGroupUser", backref="groups")
+
+
+class RelationGroupUser(db.Model):
+    __tablename__ = 'relation_group_user'
+    id = db.Column(db.Integer, primary_key=True)
+    group_id = db.Column(db.Integer, db.ForeignKey('groups.idgroups'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.idusers'))
+    admin = db.Column(db.Boolean, default=False)
