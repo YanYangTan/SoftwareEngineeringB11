99000609cd29b1bf67531999a80d354cc1414e4a
realdoonser <transfors@outlook.com>
2021-12-09 11:18:22 -0600

    roulette ui update

--- client/src/components/roulette.vue
+++ client/src/components/roulette.vue
@@ -1,14 +1,41 @@
 <template>
-  <div>
+  <div id="roulettecard">
     <el-card>
-      <el-header>
-        <p>{{chosenName}}</p>
-      </el-header>
-        <el-input placeholder="Please input" v-model="list"></el-input>
-      <el-button @click="locationPicker">Choose</el-button>
+      <div id="chosen">
+        <p>{{chosenName}}ᅠ</p>
+      </div>
+        <el-input id="entries" placeholder="Please input" v-model="list"></el-input>
+      <el-button id="choose-button" @click="locationPicker">Choose</el-button>
     </el-card>
   </div>
 </template>
+
+<style>
+#body{
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
+#roulettecard{
+  height: 90%;
+}
+.el-card{
+  height:100%;
+}
+#chosen{
+  height: 100% !important;
+  font-size: 100px;
+  text-align: center;
+  padding: 100px 0 100px 0;
+}
+#entries{
+  height: 200px !important;
+  padding: 0 0 10px 0;
+  text-align: center;
+}
+#choose-button{
+  width: 100%;
+}
+</style>
 
 <script>
 
