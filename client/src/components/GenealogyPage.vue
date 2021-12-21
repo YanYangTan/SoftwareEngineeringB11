020ef364c04eb33ae3264439eb3fcaf03de629a1
zergrisk1 <80167158+zergrisk1@users.noreply.github.com>
2021-12-22 03:47:41 +0900

    Add files via upload

--- client/src/components/GenealogyPage.vue
+++ client/src/components/GenealogyPage.vue
@@ -5,7 +5,7 @@
         <el-button type="danger" icon="el-icon-delete" @click="clearGenealogy">重置家谱</el-button>
         <el-button type="primary" icon="el-icon-check" @click="saveGenealogy">保存家谱</el-button>
       </div>
-      <el-button @click="outputUsedtable">test</el-button>
+<!--      <el-button @click="outputUsedtable">test</el-button>-->
 <!--    <div class="text-center">-->
 <!--&lt;!&ndash;      <h1 class="title">家谱</h1>&ndash;&gt;-->
 <!--    </div>-->
@@ -34,7 +34,7 @@
           </div>
         <div v-else v-for="user in userlist" :key="user.id">
 <el-menu-item index="4-1" v-if="user.used===false" @click="item.id=user.id;
-          item.defined=true;item.name=user.username;item.image='https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png';user.used = true;usedtable.push(user.id)">{{user.username}}</el-menu-item>
+          item.defined=true;item.name=user.username;item.image='https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png';user.used = true;usedtable.push(user.id);this.$forceUpdate">{{user.username}}</el-menu-item>
         </div>
       </el-menu>
     <el-menu-item  v-if="item.defined" slot="reference" index="3-2">添加成员</el-menu-item>
@@ -44,27 +44,6 @@
                      index="3-4"
      @click="customizeName(item);
             ">自定义成员</el-menu-item>
-<!--            <el-popover-->
-<!--  placement="right"-->
-<!--  v-model="seeable"-->
-<!--  width="300"-->
-<!--  trigger="click">-->
-<!--              <el-menu>-->
-<!--              <el-menu-item index="4-1" >-->
-<!--                <el-input v-model="nameinput" placeholder="请输入内容"></el-input>-->
-<!--                <el-button @click="customizeName();-->
-<!--                     item.defined=true;-->
-<!--                     item.name=nameinput;-->
-<!--                     item.image='https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png'-->
-<!--                     seeable=false;-->
-<!--                     "-->
-<!--                >确定</el-button>-->
-<!--              </el-menu-item>-->
-<!--                </el-menu>-->
-<!--                          <el-menu-item  v-if="!item.defined"-->
-<!--                     slot="reference"-->
-<!--                     index="3-4">自定义成员</el-menu-item>-->
-<!--                            </el-popover>-->
     <el-menu-item index="2-4" style="color: #dc0606" @click="addMember(item,'Delete')">删除</el-menu-item>
   </el-menu>
         <el-button slot="reference" class="custom-card"  @click="cardClick(item)">
@@ -137,12 +116,20 @@ export default {
     addMember(item, type) {
       console.log(item);
       this.test(this.tree, item.id); // 返回选中节点和层的信息
+      console.log(this.currentchildren);
       if (type === 'Sibling') {
-        console.log('addSibling');
-        // eslint-disable-next-line no-restricted-syntax
-        const ele = { firstPerson: { name: '未定义', id: this.minus, image: 's' } };
-        this.minus -= 1;
-        this.currentchildren.push(ele);
+        if (item.root === true) {
+          this.$message({
+            type: 'warning',
+            message: '根无法添加兄弟',
+          });
+        } else {
+          console.log('addSibling');
+          // eslint-disable-next-line no-restricted-syntax
+          const ele = { firstPerson: { name: '未定义', id: this.minus, image: 's' } };
+          this.minus -= 1;
+          this.currentchildren.push(ele);
+        }
       } else if (type === 'Children') {
         const ele = { firstPerson: { name: '未定义', id: this.minus, image: 's' } };
         this.minus -= 1;
@@ -177,7 +164,7 @@ export default {
           });
         } else {
           // eslint-disable-next-line no-unused-vars
-          this.test(this.currentchildren, item.id, 'Delete');
+          this.test(this.currentchildren, item.id, 'Delete', true);
           // const ttemp = [];
           // ttemp.push(this.currentchildren);
           // ttemp.splice(0, 1);
@@ -196,7 +183,7 @@ export default {
       }
     },
     // 递归遍历所有节点，根据id找到对应的children数组和节点
-    test(tree, id, type) {
+    test(tree, id, type, first) {
       this.$forceUpdate();
       let e;
       this.temptree = tree;
@@ -204,18 +191,42 @@ export default {
       for (e of this.temptree) {
         if (type === 'Delete') {
           if (e.firstPerson !== undefined) {
-            this.usedtable.splice(this.usedtable.indexOf(e.firstPerson.id), 1);
+            if (!first) {
+              this.usedtable.splice(this.usedtable.indexOf(e.firstPerson.id), 1);
+            } else if (first) {
+              this.usedtable.splice(this.usedtable.indexOf(id), 1);
+            }
           }
           if (e.secondPerson !== undefined) {
-            this.usedtable.splice(this.usedtable.indexOf(e.secondPerson.id), 1);
+            if (!first) {
+              this.usedtable.splice(this.usedtable.indexOf(e.secondPerson.id), 1);
+            } else if (first) {
+              this.usedtable.splice(this.usedtable.indexOf(id), 1);
+            }
           }
           // eslint-disable-next-line no-restricted-syntax
           for (const user of this.userlist) {
-            if (e.firstPerson !== undefined && user.id === e.firstPerson.id) {
-              user.used = false;
+            if (e.firstPerson !== undefined) {
+              if (!first) {
+                if (user.id === e.firstPerson.id) {
+                  user.used = false;
+                }
+              } else if (first) {
+                if (user.id === id) {
+                  user.used = false;
+                }
+              }
             }
-            if (e.secondPerson !== undefined && user.id === e.secondPerson.id) {
-              user.used = false;
+            if (e.secondPerson !== undefined) {
+              if (!first) {
+                if (user.id === e.secondPerson.id) {
+                  user.used = false;
+                }
+              } else if (first) {
+                if (user.id === id) {
+                  user.used = false;
+                }
+              }
             }
           }
         }
@@ -231,12 +242,13 @@ export default {
         }
         if (e.children !== undefined) { // 若它不是叶子结点
           if (type === 'Delete') {
-            this.test(e.children, id, 'Delete');
+            this.test(e.children, id, 'Delete', false);
           } else {
-            this.test(e.children, id);
+            this.test(e.children, id, false);
           } // 递归至下一个孩子层
         }
       }
+      this.$forceUpdate();
     },
     clearGenealogy() {
       this.tree = [{
