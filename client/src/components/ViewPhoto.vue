5b25a4e97fd6aedf157fd902d72ce797c4e3aa56
Johnson Hui <72008651+cantdoit@users.noreply.github.com>
2021-12-22 18:07:30 +0800

    a

--- client/src/components/ViewPhoto.vue
+++ client/src/components/ViewPhoto.vue
@@ -13,8 +13,7 @@
 
      <el-image
       style="width: 1000px; height: auto"
-      :src="info.src"
-      :fit="fit">
+      :src="info.src">
     </el-image>
 
    </el-card>
@@ -27,21 +26,27 @@
           {{info.username}}
         </h3>
       </el-main>
-
+<el-button type="success"
+                size="medium"
+                icon="el-icon-arrow-up"
+                style="padding: 1px 1px;"
+                @click="like(this.info)"
+              >{{info.like}}</el-button>
     </el-container>
 </el-container>
   </div>
   </el-container>
 </template>
-import axios from 'axios';
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'ViewPhoto',
   data() {
     return {
-      comment: '',
-      fits: ['contain'],
+      comment: {},
+      data: {},
     };
   },
   props: {
@@ -52,9 +57,49 @@ export default {
         BackToImageWall() {
           this.$emit('BacktoImageWall');
         },
+        like(pic) {
+          axios.post('/api/query-like', { post_id: pic.post_id, user_id: this.$route.params.userid }).then((res) => {
+            if (res.data.status) {
+              if (res.data.liked) {
+                axios.post('/api/cancel-like', { post_id: pic.post_id, user_id: this.$route.params.userid }).then((res2) => {
+                  if (res2.data.status) {
+                    this.$message({
+                      type: 'warning',
+                      message: '你已点赞此帖，取消点赞',
+                    });
+                    this.getPost();
+                  }
+                })
+                  .catch((err2) => {
+                    console.log(err2);
+                  });
+              } else {
+                axios.post('/api/like-post', { post_id: pic.post_id, user_id: this.$route.params.userid }).then((res2) => {
+                  if (res2.data.status) {
+                    console.log('Liked');
+                    this.$message({
+                      type: 'success',
+                      message: '成功点赞',
+                    });
+                    this.getPost();
+                  }
+                }).catch((err2) => {
+                  console.log(err2);
+                });
+              }
+            }
+          }).catch((err) => {
+            console.log(err);
+          });
+        },
+        getPost() {
+          axios.post('/api/query-all-post', { group_id: this.$props.info.id })
+        },
       },
   created() {
     console.log('success!');
+    // this.data = this.info;
+    console.log(this.info);
   },
 };
 </script>
