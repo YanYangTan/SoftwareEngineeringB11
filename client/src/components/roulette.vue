ba89754e48e5cfcdd52ceab809f3bde8d0b9e473
realdoonser <transfors@outlook.com>
2021-12-25 18:00:39 -0600

    roulette overhaul 2
    roulette undergoes a major design change, also fixes bugs where only eight values can be displayed, eight sections of the roulette can be seen at all times regardless of number of values, and text orientation being horizontal

--- client/src/components/roulette.vue
+++ client/src/components/roulette.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="roulettecard">
     <el-card>
-      <div id="wheel">
-        <div id="box" class="box">
-          <div class="box1">
-            <span class="span1"><b>{{locationNames[0]}}</b></span>
-            <span class="span2"><b>{{locationNames[1]}}</b></span>
-            <span class="span3"><b>{{locationNames[2]}}</b></span>
-            <span class="span4"><b>{{locationNames[3]}}</b></span>
-          </div>
-          <div class="box2">
-            <span class="span1"><b>{{locationNames[4]}}</b></span>
-            <span class="span2"><b>{{locationNames[5]}}</b></span>
-            <span class="span3"><b>{{locationNames[6]}}</b></span>
-            <span class="span4"><b>{{locationNames[7]}}</b></span>
-          </div>
-        </div>
-      </div>
+      <canvas id="wheel" width=504 height=504></canvas>
+      <script type="application/javascript">
+        function drawRouletteWheel() {
+          console.log("drawing roulette wheel");
+          var canvas = document.getElementById("wheel");
+          if (canvas.getContext) {
+            ctx = canvas.getContext("2d");
+            ctx.clearRect(0,0,504,504);
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 2;
+            ctx.fillStyle = "#abcdef";
+
+            ctx.beginPath();
+            ctx.arc(252, 252, 250, 0, Math.PI * 2, false);
+            ctx.stroke();
+            ctx.fill();
+          }
+        }
+        drawRouletteWheel();
+      </script>
+      <img id="arrow" src="../assets/arrow-right.png">
       <el-button id="choose-button" @click="locationPicker">转</el-button>
       <el-input id="entries" type="textarea" :autosize="{minRows:2, maxRows:9}" placeholder="Please input" v-model="list"></el-input>
     </el-card>
@@ -30,106 +35,30 @@
 }
 #wheel{
   position: relative;
-  width: 500px;
-  height: 500px;
   margin: auto;
+  display: block;
+  transition: all ease 5s;
+}
+#arrow{
+  position: absolute;
+  left: 580px;
+  top: 40%;
+  z-index: 1;
 }
 #entries{
   max-height: 200px;
   padding: 0 0 10px 0;
   text-align: center;
 }
-#wheel:before{
-  position: absolute;
-  content: '';
-  width: 32px;
-  height: 32px;
-  background: url('../assets/arrow-left.png') no-repeat;
-  background-size: 32px;
-  right: 0;
-  top: 50%;
-  transform: translateY(-25%);
-  z-index: 1;
-}
-.box{
-  width: 100%;
-  height: 100%;
-  position: relative;
-  border-radius: 50%;
-  border: 10px solid #333;
-  overflow: hidden;
-  transition: all ease 5s;
-}
-
-.span1{
-  width: 50%;
-  height: 50%;
-  display: inline-block;
-  position: absolute;
-
-  clip-path: polygon(0 92%, 100% 50%, 0 8%);
-  top: 120px;
-  left: 0;
-  background-color: #d5e8ff;
-}
-.span2{
-  width: 50%;
-  height: 50%;
-  display: inline-block;
-  position: absolute;
-
-  clip-path: polygon(100% 92%, 0 50%, 100% 8%);
-  top: 120px;
-  right: 0;
-  background-color: #ffffff;
-}
-.span3{
-  width: 50%;
-  height: 50%;
-  display: inline-block;
-  position: absolute;
-
-  clip-path: polygon(50% 0%, 8% 100%, 92% 100%);
-  bottom: 0;
-  left: 120px;
-  background-color: #a7caf5;
-}
-.span4{
-  width: 50%;
-  height: 50%;
-  display: inline-block;
-  position: absolute;
-
-  clip-path: polygon(50% 100%, 92% 0, 8% 0);
-  top: 0;
-  left: 120px;
-  background-color: #7cb4f7;
-}
-
-.box2{
-  width: 100%;
-  height: 100%;
-  transform: rotate(-135deg);
-}
-span b{
-  width: 65px;
-  height: 65px;
-  line-height: 65px;
-  border-radius: 50%;
-  font-size: 26px;
-  text-align: center;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-}
 #choose-button{
+  position: relative;
   width: 10%;
   height: 40px;
   text-align: center;
   margin: 10px auto;
   display: block;
   padding: 0px;
+  z-index: 1;
 }
 </style>
 
@@ -141,18 +70,64 @@ export default {
       list: '',
       locationNames: '',
       chosenName: '',
+      spinTimeout: null,
     };
   },
   methods: {
     locationPicker() {
+      this.locationNames = this.list.split(/, | |，|\n/);
+      const chosenNumber = Math.floor(Math.random() * this.locationNames.length);
+      this.chosenName = this.locationNames[chosenNumber];
+
+      this.drawWheel();
+
       const x = 1024;
       const y = 9999;
       const deg = Math.floor(Math.random() * (x - y)) + y;
-      document.getElementById('box').style.transform = `rotate(${deg}deg)`;
+      document.getElementById('wheel').style.transform = `rotate(${deg}deg)`;
+    },
+    drawWheel() {
+      const colors = ['#abcdef', '#d5e8ff', '#ffffff'];
+      const canvas = document.getElementById('wheel');
 
-      this.locationNames = this.list.split(/,| |，|\n/);
-      const chosenNumber = Math.floor(Math.random() * this.locationNames.length);
-      this.chosenName = this.locationNames[chosenNumber];
+      if (canvas.getContext) {
+        const ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, 504, 504);
+        ctx.strokeStyle = 'black';
+        ctx.lineWidth = 2;
+        let angle = 0;
+        const arc = ((Math.PI * 2) / this.locationNames.length);
+
+        if (this.locationNames.length < 13) ctx.font = '50px sans-serif';
+        else ctx.font = '25px sans-serif';
+
+        for (let i = 0; i < this.locationNames.length; i += 1) {
+          angle = i * arc;
+          ctx.fillStyle = colors[i % 3];
+          if (i === this.locationNames.length - 1 && i % 3 === 0) ctx.fillStyle = '#abcdef';
+
+          ctx.beginPath();
+          ctx.arc(252, 252, 250, angle, angle + arc, false);
+          ctx.stroke();
+          if (this.locationNames.length !== 1) ctx.arc(252, 252, 0, angle + arc, 0, false);
+          ctx.fill();
+          if (i === this.locationNames.length - 1) ctx.lineWidth = 1;
+          ctx.stroke();
+
+          ctx.save();
+          ctx.shadowOffsetX = -1;
+          ctx.shadowOffsetY = -1;
+          ctx.shadowBlur = 0;
+          ctx.shadowColor = 'rgb(220,220,220)';
+          ctx.fillStyle = 'black';
+          ctx.translate(250 + Math.cos(angle + arc / 2) * 160,
+            250 + Math.sin(angle + arc / 2) * 160);
+          ctx.rotate(angle + arc / 2 + Math.PI);
+          const text = this.locationNames[i];
+          ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
+          ctx.restore();
+        }
+      }
     },
   },
 };
